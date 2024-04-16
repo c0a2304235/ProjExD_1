779c11df26bf7cf71340ff337d21f2300d23c884
Ryuki <c0a2304235@edu.teu.ac.jp>
2024-04-16 16:32:53 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -17,11 +17,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x = tmr % 800
+        #print(tmr, x)
+        screen.blit(bg_img, [-x, 0])
         screen.blit(kou_img, [300, 200])   #練習4
         pg.display.update()
-        tmr += 1        
-        clock.tick(10)
+        tmr += 1
+        clock.tick(200)    #練習5
 
 
 if __name__ == "__main__":
