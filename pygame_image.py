6c2595d76762a72f952eaf2e891068f339b1d460
Ryuki <c0a2304235@edu.teu.ac.jp>
2024-04-16 17:30:10 +0900

    練習8

--- pygame_image.py
+++ pygame_image.py
@@ -11,12 +11,24 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img2 = pg.transform.flip(bg_img, True, False)
-    kou_img = pg.image.load("fig/3.png")   #練習2
-    kou_img = pg.transform.flip(kou_img, True, False)   #練習3
+    kk_img = pg.image.load("fig/3.png")   #練習2
+    kk_img = pg.transform.flip(kk_img, True, False)   #練習3
+    kk_rct = kk_img.get_rect()     #練習8-1「こうかとんRectを抽出」
+    kk_rct.center = 300, 200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
+        #print(key_lst)
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0, -1))
+        elif key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, 1))
+        elif key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1, 0))
+        elif key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0))
 
         x = tmr % 3200
         #print(tmr, x)
@@ -24,7 +36,7 @@ def main():
         screen.blit(bg_img2, [-x+1600, 0])   #練習7-1
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(bg_img2, [-x+4800, 0])    #練習7-2
-        screen.blit(kou_img, [300, 200])   #練習4
+        screen.blit(kk_img, kk_rct)   #練習4
         pg.display.update()
         tmr += 1
         clock.tick(200)    #練習5
